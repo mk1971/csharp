b797574e3aaea08e2e2a680a86410b989ddff06a
Wagner Maki <wagner.maki@hotmail.com>
2022-06-12 12:00:39 -0300

    Adding a Logo to the QR Code

---
+++ logo_qrcode/Program.cs
@@ -0,0 +1,18 @@
+﻿using IronBarCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logo_qrcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyQRWithLogo = QRCodeWriter.CreateQrCodeWithLogo("https://ironsoftware.com/csharp/barcode/", "CSharp-logo.png", 500);
+            MyQRWithLogo.SaveAsPng("Qr-Code-with-logo.png");
+        }
+    }
+}
