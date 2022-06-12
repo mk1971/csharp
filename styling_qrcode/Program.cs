fe8def63e9d120fc044d5abcb52c1ce9a4364149
Wagner Maki <wagner.maki@hotmail.com>
2022-06-12 12:13:30 -0300

    Styling a QR Code

---
+++ styling_qrcode/Program.cs
@@ -0,0 +1,20 @@
+﻿using IronBarCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace styling_qrcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyQRWithLogo = QRCodeWriter.CreateQrCodeWithLogo("https://ironsoftware.com/csharp/barcode/", "CSharp-logo.png", 500);
+            MyQRWithLogo.AddAnnotationTextAboveBarcode("Product URL:");
+            MyQRWithLogo.AddBarcodeValueTextBelowBarcode();
+            MyQRWithLogo.SaveAsPng("Qr-Code-with-Style.png");
+        }
+    }
+}
